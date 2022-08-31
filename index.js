3f5d4e8909f4802c9f420e357b1fdf08ca9a37bb
Bryan Comeau <33265715+comeaub-monotype@users.noreply.github.com>
2022-08-31 14:07:23 -0400

    Initial commit for a nodejs app that could be used to test the font security project

---
+++ index.js
@@ -0,0 +1,48 @@
+//
+// A sample node js program that leverages the puppeteer Node library and will...
+//
+//    * open up a new browser page to scrambledfonts.monotype.com
+//    * fill in the form 
+//    * submit it
+//    * delay 
+//    * close the browser 
+// 
+
+
+const puppeteer = require('puppeteer');
+    async function main() {
+        const browser = await puppeteer.launch({
+            // can be run headless by setting true
+            headless: false
+        });
+        const page = await browser.newPage();
+        await page.goto('https://scrambledfonts.monotype.com/');
+        
+       
+        // first argument can be id or class name
+        await page.type('#FirstName', 'Bryans form testing');
+        await page.type('#LastName', 'Bryans form testing');   
+        
+        await page.type('#Street', 'Bryans form testing');
+        
+        await page.type('#ZipCode', 'Bryans form testing');
+        await page.type('#City', 'Bryans form testing');
+
+        await page.type('#Country', 'Bryans form testing'); 
+        
+        await page.type('#Email', 'junk@gmail.com');
+
+        await page.type('#Password', 'SixCharacterPassword');
+
+        await page.type('#ConfirmPassword', 'SixCharacterPassword');
+
+        // find an input type that is submit
+        await page.evaluate(() => {
+            document.querySelector('input[type=submit]').click();
+        });
+
+
+        await page.waitForTimeout(10000); // wait for 5 seconds
+        await browser.close();
+    }
+    main();
